8f43476c2f68765a1658723017fa598e43ecfc39
loop-endx <loop.endx@gmail.com>
2024-01-15 11:04:18 +0800

    tracing log

--- xchannel/src/main.rs
+++ xchannel/src/main.rs
@@ -1,8 +1,11 @@
+use tracing::{error, info};
+use tracing_appender;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Registry};
+
+mod driver;
 mod drivers;
 mod error;
 mod restful;
-
-mod driver;
 mod tag;
 
 use driver::mgr;
@@ -10,17 +13,24 @@ use driver::mgr;
 use restful::REST;
 
 fn main() {
-    //    let tags = tag::Tags::new();
+    let file_appender = tracing_appender::rolling::daily("logs", "xchannel.log");
+    let (nb, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_layer = fmt::layer().with_ansi(false).with_writer(nb);
+    let stdout_layer = fmt::layer().with_writer(std::io::stdout);
 
-    //tags.add();
+    Registry::default()
+        .with(stdout_layer)
+        .with(file_layer)
+        .init();
 
     let device_mgr = mgr::DeviceMgr::init();
 
     let x = REST::new("0.0.0.0", 5260);
 
     if let Ok(x) = x {
+        info!("xchannel started.");
         x.serve(device_mgr);
     } else {
-        println!("Error: {:?}", x);
+        error!("Error: {:?}", x);
     }
 }
